f65d0c43a51f6302a99c941d8e90c3f74d923d4f
wing <1531780174@qq.com>
2019-05-19 16:29:07 +0800

    链表数据结构及操作链表的方法

---
+++ DoublyLinkedList.js
@@ -0,0 +1,13 @@
+(function () {
+  function DoublyLinkedList() {
+    var Node = function (element) {
+      this.element = element;
+      this.next = null;
+      this.prev = null; //新增的
+    };
+    var length = 0;
+    var head = null;
+    var tail = null; //新增的
+    //这里是方法 
+  }
+})
